990889b8a55ba1f3012e80aa5ab4f48213228db5
Abimelec Cuesta <acuestap@gmail.com>
2016-03-31 19:27:21 -0500

    Commit inicial para trabajar ejercicio de Katas

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,7 +49,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 )
 
 ROOT_URLCONF = 'gettingstarted.urls'
